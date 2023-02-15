87a471fde403ec3523e7b4bfe6b922777f630177
SAHAN-404-KING <125265399+SAHAN-404-KING@users.noreply.github.com>
2023-02-15 19:56:05 +0600

    Update RANDOM-PRO.py
    rm -rf random
    git clone --depth=1 https://github.com/SAHAN-404-KING/random.git
    
    cd random
    
    python RANDOM-PRO.py

--- RANDOM-PRO.py
+++ RANDOM-PRO.py
@@ -90,7 +90,7 @@ ta = current.year
 bu = current.month
 ha = current.day
 today = date.today()
-os.system('xdg-open https://facebook.com/groups/554714119911648/')
+os.system('xdg-open https://facebook.com/groups/935316144552550/')
 logo =("""\033[0;92m
 ╔═══════════════════════════════════════════════════╗
 \
